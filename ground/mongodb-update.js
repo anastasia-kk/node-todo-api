95cc0132e5af743078ac4716c69ccff3304b5e6c
anastasia-kk <anastasiya.koryakina1@gmail.com>
2018-09-20 15:16:44 +0200

    Add update script

---
+++ ground/mongodb-update.js
@@ -0,0 +1,24 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:60613/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('5b9d55e276ecf44ee11be111')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) =>{
+    console.log(result);
+  });
+
+//  db.close();
+});
